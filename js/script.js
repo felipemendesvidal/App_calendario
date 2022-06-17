4d8ded329b265d35a9bc38f491a62033a83573ee
felipe <contatofelipevidalmendes@gmail.com>
2022-06-17 11:14:15 -0300

    esqueleto funcionando

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
-var hora_sitema = new Date();
-var hora_agora = hora_sitema.getHours();
-console.log(`agora são ${hora_agora} horas`)
-if(hora_agora < 12){
-    console.log('bom dia');
-}else if(hora_agora <= 18){
-    console.log('boa tarde')
-}else{
-    console.log('boa noite')
+function carregar (){
+    var div_msg = document.getElementById('campo-msg');
+    var img = document.getElementById('imagem-relogio');
+    //pegar data atual
+    var data_atual = new Date();
+    var hora_atual = data_atual.getHours();
+    var minutos_atual = data_atual.getMinutes();
+    var fundo = document.getElementById('fundo');
+    div_msg.innerHTML = `agora são ${hora_atual} : ${minutos_atual} horas`;
+
+    //para trocar as horas
+    if(hora_atual >= 0 && hora_atual < 12){
+        //bom dia
+        img.src = 'img/manha.jpg';
+        //para mudar a cor
+        //fundo.style.background = "red";
+
+    }else if(hora_atual >= 12 && hora_atual < 18){
+        //boa tarde
+        img.src = 'img/tarde.jpg';
+
+    }else{
+        //boa noite
+        img.src = 'img/nublado.jpg';
+    }
+    console.log(fundo)
 }
+
 
